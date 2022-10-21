09bffdabd2465c6e91fe39558977dfedb4c66383
aapan-m13 <aapan.cuet.cse@gmail.com>
2022-10-21 11:23:44 +0600

    897 recursion

--- Easy/Tree/897. Increasing Order Search Tree.cpp
+++ Easy/Tree/897. Increasing Order Search Tree.cpp
@@ -48,3 +48,44 @@ public:
         
     }
 };
+
+//recursion
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        
+        vector <TreeNode*> vec;
+        inorder(root, &vec);
+        
+        for(int i = 0; i < vec.size() - 1; ++i){
+            vec[i]->right = vec[i+1];
+            vec[i]->left = NULL;
+        }
+        
+        vec[vec.size()-1]->left = NULL;
+        vec[vec.size()-1]->right = NULL;
+        
+        return vec[0];
+    }
+    
+    void inorder(TreeNode* node, vector<TreeNode*>* v){
+        
+        if(!node) return;
+        
+        inorder(node->left, v);
+        v->push_back(node);
+        inorder(node->right, v);
+    }
+};
