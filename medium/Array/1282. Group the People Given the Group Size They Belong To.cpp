b33a08a22767310c98a9e7b7789cd2ca48afcf2c
aapan-m13 <aapan.cuet.cse@gmail.com>
2022-10-28 19:24:12 +0600

    change

--- medium/Array/1282. Group the People Given the Group Size They Belong To.cpp
+++ medium/Array/1282. Group the People Given the Group Size They Belong To.cpp
@@ -6,19 +6,15 @@ public:
         vector<vector<int>> res;
         
         for(int i = 0; i < groupSizes.size(); ++i){
-            if(mp.count(groupSizes[i])){
-                if(mp[groupSizes[i]].size() == groupSizes[i]){
-                    res.push_back(mp[groupSizes[i]]);
-                    mp[groupSizes[i]] = {};
-                }
-            }
+            
             mp[groupSizes[i]].push_back(i);
+            
+            if(mp[groupSizes[i]].size() == groupSizes[i]){
+                res.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]] = {};
+            }
         }
-        
-        for(auto it = mp.begin(); it!= mp.end(); it++){
-            res.push_back(it->second);
-        }
-        
+
         return res;
         
     }
