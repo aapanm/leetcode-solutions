3ec7867b78aee556890234979e9474fbbd7b2cb5
aapan-m13 <aapan.cuet.cse@gmail.com>
2022-11-05 13:25:04 +0600

    654 o(n)

--- medium/Array/654. Maximum Binary Tree.cpp
+++ medium/Array/654. Maximum Binary Tree.cpp
@@ -41,3 +41,42 @@ public:
         return max_i;
     }
 };
+
+//o(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        
+        stack <TreeNode*> st;
+        
+        for(int i = 0; i < nums.size(); ++i){
+            
+            TreeNode* curr = new TreeNode(nums[i]);
+            
+            while(!st.empty() && st.top()->val < curr->val){
+                curr->left = st.top();
+                st.pop();
+            }
+            
+            if(!st.empty()) st.top()->right = curr;
+            
+            st.push(curr);
+        }
+        
+        while(st.size() > 1) st.pop();
+        
+        return st.top();
+    }
+};
