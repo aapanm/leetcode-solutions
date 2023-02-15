c9c890f5733cedd7d9800121f8553be0afc27ca7
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-02-15 18:39:28 +0600

    1108

---
+++ Easy/String/1108. Defanging an IP Address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        
+        string s;
+        for(auto i: address){
+            if(i=='.') s+="[.]";
+            else s+=i;
+        }
+        
+        return s;
+    }
+};
