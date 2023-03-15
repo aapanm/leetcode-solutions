8cf4070082ed9fecc379902d783df22019143fac
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-03-15 12:38:20 +0600

    682

--- Easy/Stack/682. Baseball Game.cpp
+++ Easy/Stack/682. Baseball Game.cpp
@@ -42,3 +42,28 @@ public:
         return sum;
     }
 };
+
+//vector
+
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector <int> ops;
+        for(int i=0; i<operations.size(); ++i){
+            if(operations[i]=="C"){
+                ops.pop_back();
+            }else if(operations[i]=="D"){
+                ops.push_back((ops[ops.size()-1]*2));
+            }else if(operations[i]=="+"){
+                ops.push_back(ops[ops.size()-1]+ops[ops.size()-2]);
+            }else{
+                ops.push_back(stoi(operations[i]));
+            }
+        }
+        int res=0;
+        for(int i=0; i<ops.size(); ++i){
+            res+=(ops[i]);
+        }
+        return res;
+    }
+};
