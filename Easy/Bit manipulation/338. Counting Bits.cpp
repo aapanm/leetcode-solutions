49830d1c91d25bc8a9e9cfddf676bdf3bd9fe892
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-04-08 22:54:28 +0600

    338

--- Easy/Bit manipulation/338. Counting Bits.cpp
+++ Easy/Bit manipulation/338. Counting Bits.cpp
@@ -14,3 +14,40 @@ public:
         return res;
     }
 };
+
+//one pass loop
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>res(n+1);
+        res[0] = 0;
+        for(int i=1; i<=n; ++i){
+            if(i%2==0){
+                res[i] = res[i/2];
+            }else{
+                res[i] = res[i/2] + 1;
+            } 
+        }
+        return res;
+    }
+};
+
+//one pas recursion
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>res(n+1);
+        res[0] = 0;
+        for(int i=1; i<=n; ++i){
+            res[i] = solve(i);
+        }
+        return res;
+    }
+    
+    int solve(int n){
+        if(n==0) return 0;
+        if(n%2 == 0) return solve(n/2);
+        else return solve(n/2) + 1;
+    }
+};
