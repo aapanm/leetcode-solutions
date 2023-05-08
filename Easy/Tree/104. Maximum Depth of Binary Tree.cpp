bed9879cf6edd24fcc994cb4c3416c02851138cc
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-05-08 22:29:40 +0600

    104

--- Easy/Tree/104. Maximum Depth of Binary Tree.cpp
+++ Easy/Tree/104. Maximum Depth of Binary Tree.cpp
@@ -19,6 +19,48 @@ public:
     }
 };
 
+//dfs stack
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        if(!root) return 0;
+        
+        stack <pair<TreeNode*, int>> st;
+        
+        st.push(pair(root, 1));
+        int res = INT_MIN;
+        
+        while(!st.empty()){
+            auto p = st.top();
+            st.pop();
+            
+            if(p.second > res) res = p.second;
+            
+            if(p.first->left){
+                st.push(pair(p.first->left, p.second+1));
+            }
+            if(p.first->right){
+                st.push(pair(p.first->right, p.second+1));
+            }
+        }
+        
+        return res;
+    }
+};
+
 //bfs (hash + queue)
 
 /**
