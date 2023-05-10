2f75409e5f5a7ed35683236bbe5c488070dc91b2
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-05-10 20:10:21 +0600

    226

--- Easy/Tree/226. Invert Binary Tree.cpp
+++ Easy/Tree/226. Invert Binary Tree.cpp
@@ -29,3 +29,45 @@ public:
         return root;
     }
 };
+
+//dfs stack
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        
+        if(!root) return root;
+        
+        stack <TreeNode*> st;
+        
+        st.push(root);
+        
+        while(!st.empty()){
+            
+			TreeNode* node = st.top();
+            st.pop();
+            
+			TreeNode* tmp;
+            
+			tmp = node->left;
+            node->left = node->right;
+            node->right = tmp;
+            
+			if(node->left) st.push(node->left);
+            if(node->right) st.push(node->right);
+        }
+        
+        return root;
+    }
+};
