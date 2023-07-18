dcb47fa92a049b098ab82f7aaed0499a1dd0e885
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-07-18 13:00:45 +0600

    653

---
+++ Easy/Tree/653. Two Sum IV - Input is a BST.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        
+        unordered_map <int,int> m;
+        stack <TreeNode*> st;
+        
+        st.push(root);
+        
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            if(m.count(k - node->val)) return true;
+            else m[node->val]++;
+            if(node->left) st.push(node->left);
+            if(node->right) st.push(node->right);
+        }
+        
+        return false;
+    }
+};
