cac6dd8dcfd2bb35329520a7a9ee4ca1f4917cd4
aapan-m13 <aapan.cuet.cse@gmail.com>
2023-07-30 11:53:21 +0600

    1302

---
+++ medium/Tree/1302. Deepest Leaves Sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        queue <TreeNode*> q;
+        q.push(root);
+        int s;
+        
+        while(!q.empty()){
+            int qsize = q.size();
+            s = 0;
+            
+            for(int i=0; i<qsize; ++i){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if(!node->left && !node->right) s+=node->val;
+                
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+        }
+        
+        return s;
+    }
+};
